c9bda843097159273950ea2ea87cc6395a1edae2
bluedar <128768429+bluedar@users.noreply.github.com>
2023-04-07 18:04:09 +0900

    커반 모바일 햄버거바 업데이트

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const menu_wrap = document.querySelector('.menu_wrap')
 const hamburg_bar = document.querySelector('.hamburg_bar')
 const dark = document.querySelector('.dark')
+const hamburg_line = document.querySelector('.hamburg_line')
 
 let hamburg_btn = 0
 hamburg_bar.addEventListener('click',function(){
@@ -9,11 +10,13 @@ hamburg_bar.addEventListener('click',function(){
     menu_wrap.style.right='0'
     dark.style.display='block'
     dark.style.background= 'rgba(0,0,0,0.7)';
+    hamburg_line.classList.add('on')
     hamburg_btn = 'on'
   }else{
     menu_wrap.style.right='-55%'
     dark.style.display='none'
     dark.style.background= 'rgba(0,0,0,0)';
+    hamburg_line.classList.remove('on')
     hamburg_btn = 0
   }
 })
