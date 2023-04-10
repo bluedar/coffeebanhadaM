126d0b99db5f23ca3c0d6a4d7ee4eb9b26ff265c
bluedar <128768429+bluedar@users.noreply.github.com>
2023-04-10 16:44:54 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -20,8 +20,13 @@ hamburg_bar.addEventListener('click',function(){
     hamburg_btn = 0
   }
 })
-
-
+dark.addEventListener('click',function(){
+  menu_wrap.style.right='-55%'
+    dark.style.display='none'
+    dark.style.background= 'rgba(0,0,0,0)';
+    hamburg_line.classList.remove('on')
+    hamburg_btn = 0
+})
 
 $('.main_menu_wrap').on('click',function(){
   $('.sub_menu_wrap').slideUp()
